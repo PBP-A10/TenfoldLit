4b27f8203d22e6e2003ae795ac7d86206fc0da2f
nabielsubhan <mnsubhan1509@gmail.com>
2023-12-16 21:32:39 +0700

    minor update

--- friends/views.py
+++ friends/views.py
@@ -18,7 +18,6 @@ def view_friends(request):
     return render(request, 'friends.html', {'friends': friends, 'current_user': request.user})
 
 @login_required
-@csrf_exempt
 def follow_friend(request, friend_id):
     if request.method == 'POST':
         user_connections = UserConnections.objects.get(user=request.user)
@@ -34,6 +33,41 @@ def unfollow_friend(request, friend_id):
         friend = UserConnections.objects.get(pk=friend_id)
         user_connections.remove_friend(friend)
         return HttpResponse(b"UNFOLLOWED", status=201)
+    return HttpResponseNotFound()
+
+@login_required
+@csrf_exempt
+def follow_friend_flutter(request, friend_id):
+    if request.method == 'POST':
+        user_connections = UserConnections.objects.get(user=request.user)
+        friend = UserConnections.objects.get(pk=friend_id)
+        user_connections.add_friend(friend)
+        
+        response_data = {"status": "success", "message": "User followed successfully"}
+        return JsonResponse(response_data, status=201)
+    return HttpResponseNotFound()
+
+@login_required
+@csrf_exempt
+def unfollow_friend_flutter(request, friend_id):
+    if request.method == 'POST':
+        user_connections = UserConnections.objects.get(user=request.user)
+        friend = UserConnections.objects.get(pk=friend_id)
+        user_connections.remove_friend(friend)
+
+        response_data = {"status": "success", "message": "User unfollowed successfully"}
+        return JsonResponse(response_data, status=201)
+    return HttpResponseNotFound()
+
+@login_required
+def unfollow_friend(request, friend_id):
+    if request.method == 'POST':
+        user_connections = UserConnections.objects.get(user=request.user)
+        friend = UserConnections.objects.get(pk=friend_id)
+        user_connections.remove_friend(friend)
+
+        response_data = {"status": "success", "message": "User unfollowed successfully"}
+        return JsonResponse(response_data, status=201)
     return HttpResponseNotFound()
 
 @login_required
@@ -58,20 +92,18 @@ def get_user_connections(request, user_id):
 @login_required
 def get_friends_user_object(request, user_connection_id):
     connection = UserConnections.objects.get(pk=user_connection_id)
-
-    user_ids = connection.friends.values_list('user_id', flat=True)
-
-    users = User.objects.filter(pk__in=user_ids)
-
+    friends = connection.get_friends()
+    
     user_list = [
         {
-            "id": user.pk,
-            "password": user.password,
-            "last_login": user.last_login,
-            "username": user.username,
-            "email": user.email,
+            "user_connection_id": entry.pk,
+            "user_id": entry.user.pk,
+            "password": entry.user.password,
+            "last_login": entry.user.last_login,
+            "username": entry.user.username,
+            "email": entry.user.email,
         }
-        for user in users
+        for entry in friends
     ]
 
     return JsonResponse(user_list, safe=False)
@@ -95,14 +127,34 @@ def get_all_users(request):
 
 @login_required
 def get_current_user(request):
+    user_connection = UserConnections.objects.get(user=request.user)
     user_list = [
         {
-            "id": request.user.pk,
+            "user_connection_id": user_connection.pk,
+            "user_id": request.user.id,
             "password": request.user.password,
             "last_login": request.user.last_login,
             "username": request.user.username,
             "email": request.user.email,
         }
+    ]
+
+    return JsonResponse(user_list, safe=False)
+
+@login_required
+def get_all_user_connections_object(request):
+    connections = UserConnections.objects.all()
+
+    user_list = [
+        {
+            "user_connection_id": entry.pk,
+            "user_id": entry.user.pk,
+            "password": entry.user.password,
+            "last_login": entry.user.last_login,
+            "username": entry.user.username,
+            "email": entry.user.email,
+        }
+        for entry in connections
     ]
 
     return JsonResponse(user_list, safe=False)
